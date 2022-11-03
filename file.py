bf62fc2041d75060a72118425fb83c1c321b7f54
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-11-03 17:03:38 +0800

    兩種讀+寫檔案的格式 python

--- file.py
+++ file.py
@@ -3,12 +3,17 @@
 # #讀取檔案
 # file.write("Hello File\nSecond Line\n測試")
 # file.close()
-with open("data.txt", mode="w", encoding="utf-8") as file:
-    file.write("Hello File\nSecond Line\n測試111")
+# with open("data.txt", mode="w", encoding="utf-8") as file:
+#     file.write("5\n3")
 
-#讀取檔案
-with open("data.txt", mode="r", encoding="utf-8") as file:
-        data=file.read()
-print(data)
+# #讀取檔案
+# sum=0
+# with open("data.txt", mode="r", encoding="utf-8") as file:
+#         # data=file.read()
+#         for line in file:
+#             sum=sum+int(line)
+
+# print(sum)
 
 #使用Json 格式讀取
+
