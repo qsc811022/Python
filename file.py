21dd57851911e9b547d4ca458b74440fc5dd7f10
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-11-03 16:49:06 +0800

    with open("data.txt", mode="r", encoding="utf-8") as file:         data=file.read() print(data)

--- file.py
+++ file.py
@@ -1,7 +1,14 @@
-#儲存檔案
-file = open("data.txt", mode="w")
+# #儲存檔案
+# file = open("data.txt", mode="w", encoding="utf-8")
+# #讀取檔案
+# file.write("Hello File\nSecond Line\n測試")
+# file.close()
+with open("data.txt", mode="w", encoding="utf-8") as file:
+    file.write("Hello File\nSecond Line\n測試111")
+
 #讀取檔案
-file.write("Hello File")
-file.close()
+with open("data.txt", mode="r", encoding="utf-8") as file:
+        data=file.read()
+print(data)
 
 #使用Json 格式讀取
