781bc8c6b8fcab56a9add8e0a0089f8bb959d7c0
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-11-03 15:30:24 +0800

    Modify python read file

---
+++ file.py
@@ -0,0 +1,7 @@
+#儲存檔案
+file = open("data.txt", mode="w")
+#讀取檔案
+file.write("Hello File")
+file.close()
+
+#使用Json 格式讀取
---
+++ module.py
@@ -0,0 +1,8 @@
+import geometry
+
+
+result=geometry.distance(1,1,5,5)
+print(result)
+
+result1=geometry.slop(1,2,5,6)
+print(result1)
