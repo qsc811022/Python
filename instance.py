474f06a8a06c9f7c11c3aa2a9fc97f880292256d
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-11-08 20:40:35 +0800

    定義類別 產生實體物件

---
+++ instance.py
@@ -0,0 +1,21 @@
+# class Point:
+#     def __init__(self,x,y) -> None:
+#         self.x=x
+#         self.y=y
+
+# #建立一個實體物件
+# p1=Point(3,4)
+# print(p1.x, p1.y)
+# #印出來
+
+# #建立第二個實體物件
+# p2=Point(5,6)
+# print(p2.x,p2.y)
+
+class FunllName:
+    def __init__(self,first, last) -> None:
+        self.first=first
+        self.last=last
+
+name1=FunllName("aaa","ffff")
+print(name1.first,name1.last)
