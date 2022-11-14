996345a6673a02116908367cf655e8567e0dac76
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-11-14 10:14:44 +0800

    Python 網路爬蟲 Web Crawler 基本教學

---
+++ crawler.py
@@ -0,0 +1,22 @@
+import urllib.request as req
+url="https://www.ptt.cc/bbs/movie/index.html"
+
+#建立一個 Requestion物件，附加Request Headers的資訊
+request=req.Request(url, headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+})
+
+with req.urlopen(request) as response:
+    data=response.read().decode("utf-8")
+
+#print(data)
+
+import bs4
+root=bs4.BeautifulSoup(data,"html.parser")
+#print(root.title.string)
+titles=root.find("div",class_="title") #尋找所有clas="Title的"
+#print(titles.a.string)
+for title in titles:
+    
+    if title.a != None:
+        print(title.a.string)
